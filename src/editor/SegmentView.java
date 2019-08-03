e0462e902ec0933d07d837dc82996bcd838397ec
Olof Andersson <olof.andersson@gmail.com>
2019-08-03 11:27:16 +0200

    scroll to segment on key pressed

--- src/editor/SegmentView.java
+++ src/editor/SegmentView.java
@@ -327,6 +327,11 @@ public class SegmentView extends javax.swing.JPanel {
                 markupViewTargetMouseReleased(evt);
             }
         });
+        markupViewTarget.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                markupViewTargetKeyPressed(evt);
+            }
+        });
         jScrollPane4.setViewportView(markupViewTarget);
 
         jPanel1.add(jScrollPane4);
@@ -407,7 +412,6 @@ public class SegmentView extends javax.swing.JPanel {
 		if (modifiedFlag) {
 			updateHeight();
 		}
-		fileView.scroll_to_segment(this);
 		modifiedFlag = false;
     }//GEN-LAST:event_markupViewTargetCaretUpdate
 
@@ -462,6 +466,10 @@ public class SegmentView extends javax.swing.JPanel {
 			showTargetPopup(evt);
 		}
     }//GEN-LAST:event_markupViewTargetMouseReleased
+
+    private void markupViewTargetKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_markupViewTargetKeyPressed
+		fileView.scroll_to_segment(this);
+    }//GEN-LAST:event_markupViewTargetKeyPressed
 
 	void setEditorFont(Font f, int minHeight) {
 		this.minHeight = minHeight;
