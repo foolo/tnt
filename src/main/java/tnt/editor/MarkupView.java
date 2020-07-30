352694d060e829407e831df8303f588994872e25
Olof Andersson <olof.andersson@gmail.com>
2020-07-30 14:23:24 +0200

    rm

--- src/main/java/tnt/editor/MarkupView.java
+++ src/main/java/tnt/editor/MarkupView.java
@@ -1,10 +1,6 @@
 package tnt.editor;
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import javax.swing.JTextPane;
-import javax.swing.TransferHandler;
 import javax.swing.text.Caret;
 import javax.swing.text.DefaultCaret;
 import javax.swing.text.DefaultHighlighter;
@@ -29,22 +25,10 @@ public class MarkupView extends JTextPane {
 		return segmentView;
 	}
 
-	@Override
-	public final void setTransferHandler(TransferHandler newHandler) {
-		super.setTransferHandler(newHandler);
-	}
-
 	public void setTaggedText(String s) {
 		int caretPosition = getCaretPosition();
 		setText(s);
 		setCaretPosition(Math.min(caretPosition, getText().length()));
-	}
-
-	@Override
-	public void paintComponent(Graphics g) {
-		Graphics2D g2 = (Graphics2D) g;
-		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-		super.paintComponent(g);
 	}
 
 	@Override
